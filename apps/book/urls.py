17853f072fdbbc1fa14c31598b69da13c4888cc0
shehab0717 <shehab.abdullah12@gmail.com>
2026-01-19 15:07:31 +0200

    update urls names

--- apps/book/urls.py
+++ apps/book/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "book"
 urlpatterns = [
     # path("", views.list, name="index"),
-    path("", views.BookListView.as_view(), name="index"),
-    path("<int:book_isbn>/", views.BookDetailView.as_view(), name="detail"),
-    path("<int:book_isbn>/update/", views.UpdateBookView.as_view(), name="update"),
-    path("add/", views.AddBookView.as_view(), name="add"),
+    path("", views.BookListView.as_view(), name="book_index"),
+    path("<int:book_isbn>/", views.BookDetailView.as_view(), name="book_detail"),
+    path("<int:book_isbn>/update/", views.UpdateBookView.as_view(), name="book_update"),
+    path("add/", views.AddBookView.as_view(), name="book_add"),
 ]
