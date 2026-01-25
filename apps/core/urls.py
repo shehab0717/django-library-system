66df14b432df484b1858c860f7e6f71128b76323
Shehab Abdullah <shehab.abdullah12@gmail.com>
2026-01-25 22:37:37 +0200

    add: logout feature

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 from django.urls import path
 from . import views
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path("user/login/", LoginView.as_view(next_page="/user/profile/"), name="login"),
     path("user/profile/", views.UserProfileView.as_view(), name="user_profile"),
+    path("user/logout", LogoutView.as_view(next_page="/"), name="logout"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
