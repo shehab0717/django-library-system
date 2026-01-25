2e7e749cd77939041d562fd6e5f9e45d2bc060f9
shehab0717 <shehab.abdullah12@gmail.com>
2026-01-25 21:18:25 +0200

    add: active link context processor

---
+++ utils/context_processor.py
@@ -0,0 +1,13 @@
+def active_link(request):
+    url: str = request.path
+    print(url)
+    if url.startswith("/book/author"):
+        return {"active_link": "authors"}
+    if url.startswith("/book/"):
+        return {"active_link": "books"}
+    if url.startswith("/member/"):
+        return {"active_link": "members"}
+    if url.startswith("/circulation/"):
+        return {"active_link": "circulations"}
+
+    return {"active_link": "none"}
