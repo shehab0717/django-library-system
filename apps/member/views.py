de955bd73ef7438e2bde872465e2b8788345ad94
Shehab Abdullah <shehab.abdullah12@gmail.com>
2026-02-11 20:29:19 +0200

    set authorization on member operations

--- apps/member/views.py
+++ apps/member/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import View
+from django.contrib.auth.models import User
 from . import models
 from .forms import MemberCreateForm
 from django.contrib.auth.mixins import PermissionRequiredMixin
@@ -11,8 +12,16 @@ class MmeberIndexView(PermissionRequiredMixin, View):
     permission_required = "member.view_member"
 
     def get(self, request):
+        member_add_perm = request.user.has_perm("member.add_member")
         members = models.Member.objects.all()
-        return render(request, "member/member_index.html", {"members": members})
+        return render(
+            request,
+            "member/member_index.html",
+            {
+                "members": members,
+                "perms": {"member_add": member_add_perm},
+            },
+        )
 
 
 class MmemberCreateView(PermissionRequiredMixin, View):
@@ -34,5 +43,10 @@ class MemberDetailView(PermissionRequiredMixin, View):
     permission_required = "member.view_member"
 
     def get(self, request, member_id):
+        borrow_update_perm = request.user.has_perm("member.add_member")
         member = get_object_or_404(models.Member, pk=member_id)
-        return render(request, "member/member_detail.html", {"member": member})
+        return render(
+            request,
+            "member/member_detail.html",
+            {"member": member, "perms": {"borrow_update": borrow_update_perm}},
+        )
